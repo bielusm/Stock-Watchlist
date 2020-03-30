97f748da0b34abe5ac19397d833b2e7ee146c2fb
Mitchell Bielus <mitchellbielus@gmail.com>
2020-03-30 14:05:46 -0500

    added getExtremes and updated getLast52Data

--- server/tests/stocks/stockCalculations.test.js
+++ server/tests/stocks/stockCalculations.test.js
@@ -1,4 +1,7 @@
-const { getLast52Data } = require('../../stocks/stockCalculations');
+const {
+  getExtremes,
+  getLast52Data
+} = require('../../stocks/stockCalculations');
 const ibmData = require('../fixtures/stocks/ibmStocks.json');
 
 describe('stockCalculation tests', () => {
@@ -7,6 +10,16 @@ describe('stockCalculation tests', () => {
       const last52 = getLast52Data(ibmData['Weekly Time Series']);
       expect(last52).toHaveLength(52);
       expect(last52).toMatchSnapshot();
+    });
+  });
+
+  describe('getExtremes', () => {
+    test('gets highest and lowest value', () => {
+      const last52 = require('../fixtures/stocks/last52.json');
+
+      const { lowest, highest } = getExtremes(last52);
+      expect(lowest).toBeCloseTo(90.56);
+      expect(highest).toBeCloseTo(158.75);
     });
   });
 });
